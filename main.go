ffc0eca0ec0ff17a61b9bb1d85615faacd88ac5a
Deivison <deivison.rocha@mercadolivre.com>
2023-01-31 21:20:50 -0300

    criação do arquivo main.go

---
+++ main.go
@@ -0,0 +1,23 @@
+package area
+
+import "math"
+
+// Pi é uma proporção numeroa defina pela relação entre
+// o perimetro de uma circunferencia e seu diametro
+const Pi = 3.1416
+
+// Circ é responsavel por calcular a área da circunferencia
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) / Pi
+}
+
+// Rect é responsavel por calcular a área de um retangulo
+func Rect(base, altura float64) float64 {
+	return base * altura
+
+}
+
+// Não é visivel
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+}
